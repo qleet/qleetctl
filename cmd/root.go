0a04c75376b6fcd73c9305b62710eea166906692
Rich Lander <lander2k2@protonmail.com>
2022-11-09 07:46:37 -0500

    feat: fix and improve the help info
    Signed-off-by: Rich Lander <lander2k2@protonmail.com>

--- cmd/root.go
+++ cmd/root.go
@@ -14,7 +14,7 @@ import (
 var rootCmd = &cobra.Command{
 	Use:   "qleetctl",
 	Short: "Manage QleetOS",
-	Long: `Qleet OS is a global control plane for your softare.  The qleetctl
+	Long: `Qleet OS is a global control plane for your software.  The qleetctl
 CLI installs and manages instances of the QleetOS control plane as well as
 applications that are deployed into the QleetOS compute space.`,
 	// Uncomment the following line if your bare application
@@ -32,13 +32,5 @@ func Execute() {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.qleetctl.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
--- cmd/version.go
+++ cmd/version.go
@@ -17,13 +17,8 @@ var version string
 // versionCmd represents the version command
 var versionCmd = &cobra.Command{
 	Use:   "version",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Print the version of qleetctl",
+	Long:  `Print the version of qleetctl.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println(version)
 	},
@@ -31,14 +26,4 @@ to quickly create a Cobra application.`,
 
 func init() {
 	rootCmd.AddCommand(versionCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// versionCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// versionCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
