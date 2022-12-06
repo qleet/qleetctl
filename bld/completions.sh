012b782f4fe06d46040182c6e6e580c001a02986
Andriy Kalashnykov <AndriyKalashnykov@gmail.com>
2022-12-05 21:19:32 -0500

    fix: updated completions
    Signed-off-by: Andriy Kalashnykov <AndriyKalashnykov@gmail.com>

--- bld/completions.sh
+++ bld/completions.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
-set -e
+set -ex
 rm -rf completions
 mkdir completions
-pwd
-ls -al
-ls -al ../
+
 for sh in bash zsh fish; do
 	go run ./main.go completion "$sh" >"completions/qleetctl.$sh"
 done
