fe3271fcdba37e045561ddb7334e2a5ae732eca5
Andriy Kalashnykov <AndriyKalashnykov@gmail.com>
2022-12-05 20:31:59 -0500

    fix: updated completions generation
    Signed-off-by: Andriy Kalashnykov <AndriyKalashnykov@gmail.com>

--- build/completions.sh
+++ build/completions.sh
@@ -3,6 +3,7 @@ set -e
 rm -rf completions
 mkdir completions
 for sh in bash zsh fish; do
-	go run ./main.go completion "$sh" >"completions/qleetctl.$sh"
+  qleetctl completion "$sh" >"completions/qleetctl.$sh"
+#	go run ./main.go completion "$sh" >"completions/qleetctl.$sh"
 done
 chmod +x ./completions/
